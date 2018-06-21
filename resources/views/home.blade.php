0aa3df2bfb6edfa8f7c1ee2133e644b374ade0bd
artem <artdanfree@yandex.ru>
2018-06-21 19:47:54 +0300

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,23 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Laravel Filemanager</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('vendor/laravel-filemanager/img/folder.png') }}">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
 <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
 
-    <div class="row">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-        <div class="col-md-6">
-            <h2>Standalone Image Button</h2>
-            <div class="input-group">
-          <span class="input-group-btn">
-            <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-              <i class="fa fa-picture-o"></i> Choose
-            </a>
-          </span>
-                <input id="thumbnail" class="form-control" type="text" name="filepath">
+                    You are logged in!
+                </div>
             </div>
-            <img id="holder" style="margin-top:15px;max-height:100px;">
-            <h2>Standalone File Button</h2>
-            <div class="input-group">
-          <span class="input-group-btn">
-            <a id="lfm2" data-input="thumbnail2" data-preview="holder2" class="btn btn-primary">
-              <i class="fa fa-picture-o"></i> Choose
-            </a>
-          </span>
-                <input id="thumbnail2" class="form-control" type="text" name="filepath">
-            </div>
-            <img id="holder2" style="margin-top:15px;max-height:100px;">
         </div>
     </div>
-
 </div>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>
-    var route_prefix = "{{ url(config('lfm.url_prefix', config('lfm.prefix'))) }}";
-</script>
-
-
-<script>
-    {!! \File::get(base_path('vendor/unisharp/laravel-filemanager/public/js/lfm.js')) !!}
-</script>
-<script>
-    $('#lfm').filemanager('image', {prefix: route_prefix});
-    $('#lfm2').filemanager('file', {prefix: route_prefix});
-</script>
-</body>
-</html>
+@endsection
